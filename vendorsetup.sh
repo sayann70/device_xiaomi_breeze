122c11f15cabda7cdb353aeac7cdd5f8988c6e48
sayann70 <sayansarkarda2@gmail.com>
2025-05-04 14:49:41 +0530

    breeze: Import vendorsetup
    Signed-off-by: sayann70 <sayansarkarda2@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Kernel & Vendor Sources
+git clone https://github.com/xiaomi-sm4450-devs/vendor_xiaomi_breeze vendor/xiaomi/breeze
+git clone https://github.com/xiaomi-sm4450-devs/device_xiaomi_breeze-kernel device/xiaomi/breeze-kernel
+
+# Hardware/Xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
