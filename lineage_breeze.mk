6cb78b94c7063a8d28355afa4116368d846a02b8
Niyush-04 <mauryapradip04@gmail.com>
2025-12-21 16:31:10 +0530

    breeze: Update from OS3.0.10.0.WNUINXM

--- lineage_breeze.mk
+++ lineage_breeze.mk
@@ -29,8 +29,8 @@ PRODUCT_MODEL := 24066PC95I
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="breeze-user 15 AQ3A.240912.001 OS2.0.204.0.VNUINXM release-keys" \
-    BuildFingerprint=POCO/breeze_p_in/breeze:15/AQ3A.240912.001/OS2.0.204.0.VNUINXM:user/release-keys \
+    BuildDesc="breeze-user 16 BP2A.250605.031.A3 OS3.0.10.0.WNUINXM release-keys" \
+    BuildFingerprint=POCO/breeze_p_in/breeze:16/BP2A.250605.031.A3/OS3.0.10.0.WNUINXM:user/release-keys \
     DeviceName=breeze \
     DeviceProduct=breeze_p_in
 
